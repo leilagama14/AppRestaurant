c0b4aa4845a08fcd2cedcd255d3c5c35f4493664
leilagama14 <leilagama@gmail.com>
2022-03-07 17:02:56 -0300

    manutenção na OrderCntroller

--- Controllers/DishTypeController.cs
+++ Controllers/DishTypeController.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using AppRestaurant.Models;
 
 namespace AppRestaurant.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class DishTypeController : ControllerBase
     {
         [HttpGet]
         public IEnumerable<DishType> Get()
--- Controllers/TimeDayController.cs
+++ Controllers/TimeDayController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using AppRestaurant.Models;
 
 namespace AppRestaurant.Controllers
 {
