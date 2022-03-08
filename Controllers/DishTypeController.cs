bbaacd65117c3c2fc502f41b2cb4334e97e7781d
leilagama14 <leilagama@gmail.com>
2022-03-08 15:08:37 -0300

    Alter controller

--- Controllers/DishTypeController.cs
+++ Controllers/DishTypeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AppRestaurant.Data;
+using AppRestaurant.Data.DTOs.DishType;
 using AppRestaurant.Models;
 
 namespace AppRestaurant.Controllers
@@ -13,10 +14,33 @@ namespace AppRestaurant.Controllers
     [Route("[controller]")]
     public class DishTypeController : ControllerBase
     {
+        private RestaurantContext _context;
+
+        public DishTypeController(RestaurantContext context){
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AddDish([FromBody] DishType dishtype){
+            _context.DishTypes.Add(dishtype);
+            _context.SaveChanges();
+            return CreatedAction(nameof(RecoverDishTypeId), new {Id = dishtype.Id}, dishtype)                
+        }
+
         [HttpGet]
-        public IEnumerable<DishType> RecoverDishType()
+        public IActionResult RecoverDishType()
         {
-            return null;
+            return Ok(_context.DishTypes);
+        }
+
+        [HttpGet("{id}")]
+        public DishType RecoverDishTypeId(int id)
+        {
+            DishType dishtype = _context.DishTypes.FirstOrDefault(dishtype => dishtype.Id == id)
+            if(dishtype != null){
+                return Ok(dishtype);
+            }
+            return NotFound();
         }
 
     }
